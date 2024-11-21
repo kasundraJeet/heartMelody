6eeda723644ed0239f94385a3394e4c5717446d9
kasundraJeet <kasundrajeet29@gmail.com>
2024-11-21 08:44:15 +0530

    addded new components

--- apps/panel/pages/artist.vue
+++ apps/panel/pages/artist.vue
@@ -1,3 +1,25 @@
+<script setup>
+import { toTypedSchema } from '@vee-validate/zod'
+import { useForm } from 'vee-validate'
+import * as z from 'zod'
+
+const formSchema = toTypedSchema(z.object({
+    name: z.string().min(2).max(50),
+    fruits: z.array(z.string()).min(1).max(3),
+}))
+
+const { isFieldDirty, handleSubmit } = useForm({
+    validationSchema: formSchema,
+    initialValues: {
+        fruits: ['Apple', 'Banana'],
+    },
+})
+
+const onSubmit = handleSubmit((values) => {
+    console.log(values)
+})
+</script>
+
 <template>
     <div class="p-3.5 space-y-12">
         <div class="flex items-center justify-between">
@@ -25,12 +47,14 @@
             </div>
         </div>
         <div class="w-full">
-            <div class="w-full">
+            <div class="w-full space-y-8">
                 <ul class="w-full grid grid-cols-5 gap-5">
                     <ContextMenu as="li">
-                        <ContextMenuTrigger class="artist-box group">
-                            <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRr2C7vN85FNbwRSVH1BGWAl0DRlvDccmWbsb9nhV7mrNYyUcBO"
-                                alt="name" />
+                        <ContextMenuTrigger>
+                            <div class="artist-box">
+                                <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRr2C7vN85FNbwRSVH1BGWAl0DRlvDccmWbsb9nhV7mrNYyUcBO"
+                                    alt="name" />
+                            </div>
                         </ContextMenuTrigger>
                         <ContextMenuContent class="w-56">
                             <ContextMenuItem>
@@ -83,9 +107,62 @@
                         </ContextMenuContent>
                     </ContextMenu>
                 </ul>
+                <div class="flex items-center justify-center gap-3">
+                    <Button variant="outline">
+                        <Icon name="ri:loader-2-line" class="w-5 h-5" />
+                        Show More
+                    </Button>
+                </div>
             </div>
         </div>
     </div>
+    <Sheet :open="false">
+        <SheetContent class="flex flex-col p-0">
+            <SheetHeader class="p-3 pb-0">
+                <SheetTitle>Edit profile</SheetTitle>
+                <SheetDescription>
+                    Make changes to your profile here. Click save when you're done.
+                </SheetDescription>
+            </SheetHeader>
+            <form class="w-full h-full flex flex-col justify-between" @submit="onSubmit">
+                <ScrollArea class="h-full w-full overflow-x-visible">
+                    <div class="space-y-3 overflow-x-visible p-3">
+                        <FormField v-slot="{ componentField }" name="name" :validate-on-blur="!isFieldDirty">
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input type="text" placeholder="shadcn" v-bind="componentField" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        </FormField>
+                        <FormField v-slot="{ value }" name="fruits">
+                            <FormItem>
+                                <FormLabel>Fruits</FormLabel>
+                                <FormControl>
+                                    <TagsInput :model-value="value">
+                                        <TagsInputItem v-for="item in value" :key="item" :value="item">
+                                            <TagsInputItemText />
+                                            <TagsInputItemDelete />
+                                        </TagsInputItem>
+
+                                        <TagsInputInput placeholder="Fruits..." />
+                                    </TagsInput>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        </FormField>
+                    </div>
+                </ScrollArea>
+                <div class="p-3 w-full">
+                    <Button type="submit" class="w-full">
+                        Submit
+                    </Button>
+                </div>
+            </form>
+        </SheetContent>
+    </Sheet>
+
 </template>
 
 
